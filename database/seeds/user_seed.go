3bd20d934c0ab0f85fcc2a8ab38e1bb6d44f5f25
HarisPratama <hpratama324@gmail.com>
2026-01-21 14:26:58 +0700

    adding Dockerfile

--- database/seeds/user_seed.go
+++ database/seeds/user_seed.go
@@ -1,24 +1,28 @@
 package seeds
 
 import (
+	"bwanews/internal/core/domain/model"
+	"bwanews/lib/conv"
+
+	"github.com/rs/zerolog/log"
 	"gorm.io/gorm"
 )
 
 func SeedRoles(db *gorm.DB) {
-	//bytes, err := conv.HashPassword("admin123")
-	//if err != nil {
-	//	log.Fatal().Err(err).Msg("Failed to generate password")
-	//}
+	bytes, err := conv.HashPassword("admin123")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to generate password")
+	}
 
-	//admin := model.User{
-	//	Name:     "Admin",
-	//	Email:    "admin@mail.com",
-	//	Password: string(bytes),
-	//}
+	admin := model.User{
+		Name:     "Admin",
+		Email:    "admin@mail.com",
+		Password: string(bytes),
+	}
 
-	//if err := db.FirstOrCreate(&admin, model.User{Email: "admin@mail.com"}).Error; err != nil {
-	//	log.Fatal().Err(err).Msg("Failed to create user")
-	//} else {
-	//	log.Info().Msg("Admin role seeded successfully")
-	//}
+	if err := db.FirstOrCreate(&admin, model.User{Email: "admin@mail.com"}).Error; err != nil {
+		log.Fatal().Err(err).Msg("Failed to create user")
+	} else {
+		log.Info().Msg("Admin role seeded successfully")
+	}
 }
